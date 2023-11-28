ba5e7fd748070996a0b46a60228f83a2721659fe
Alex_de_code <alexandergonzalez@pursuit.orgclear>
2023-11-28 16:57:04 -0500

    added a deleteTodo function/event listener so that delete button can have functionality

--- src/App.jsx
+++ src/App.jsx
@@ -15,6 +15,7 @@ export default function App() {
     })
     setNewItem("")
   }
+
   function toggleTodo(id, completed) {
     setTodos(currentTodos => {
       return currentTodos.map(todo => {
@@ -23,6 +24,12 @@ export default function App() {
         }
         return todo
       })
+    })
+  }
+
+  function deleteTodo(id) {
+    setTodos(currentTodos => {
+      return currentTodos.filter(todo => todo.id !== id)
     })
   }
 
@@ -44,7 +51,7 @@ export default function App() {
           <input type="checkbox" checked={todocompleted} onChange={e => toggleTodo(todo.id, e.target.checked)}/>
           {todo.title}
         </label>
-        <button className="btn btn-danger">Delete</button>
+        <button className="btn btn-danger" onClick={() => deleteTodo(todo.id)}>Delete</button>
       </li>
     )
     })}
