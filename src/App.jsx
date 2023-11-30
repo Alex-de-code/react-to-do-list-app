0b04dd3996b39923a92fea14e8aced08923babc8
Alex_de_code <alexandergonzalez@pursuit.orgclear>
2023-11-29 21:13:24 -0500

    used useEffect hook to and added a listener to store everything in local storage

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import "../src/styles.css"
 import { NewTodoForm } from "./NewTodoForm"
 import { TodoList } from "./TodoList"
 
 export default function App() {
-  const [newItem, setNewItem] = useState("")
-  const [todos, setTodos] = useState([])
+  // const [newItem, setNewItem] = useState("")
+  const [todos, setTodos] = useState(() => {
+    const localValue = localStorage.getItem("ITEMS")
+    if (localValue == null) return []
+    return JSON.parse(localValue)
+  })
+
+  useEffect(() => {
+    localStorage.setItem("ITEMS", JSON.stringify(todos))
+  }, [todos])
 
   function addTodo(newItem){
     setTodos((currentTodos) => {
